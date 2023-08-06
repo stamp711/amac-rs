cc7dcb5d65fd5677ea3a03d3ba0c57bfb0132ad9
Apricity <stamp1024@gmail.com>
2023-08-06 17:35:25 +0000

    add generators

--- benches/binary_search.rs
+++ benches/binary_search.rs
@@ -1,6 +1,15 @@
-use amac::{async_load, LocalPool};
+#![feature(generators, generator_trait)]
+
+use std::{
+    ops::{Generator, GeneratorState},
+    pin::Pin,
+    task::Poll,
+};
+
+use amac::{async_load, prefetch, LocalPool};
 use clf::cache_line_flush_with_slice;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use futures::Future;
 
 fn gen_vec(len: usize) -> Vec<i32> {
     let mut vec = Vec::with_capacity(len);
@@ -43,14 +52,45 @@ fn binary_search_inner(v: &[i32], target: i32) {
 }
 
 fn binary_search_amac(v: &[i32], targets: &[i32]) {
-    let mut futs = vec![];
-    for t in targets {
-        let fut = binary_search_amac_inner(v, *t);
-        futs.push(Box::pin(fut));
-    }
+    let futs = targets
+        .iter()
+        .map(|target| Box::pin(binary_search_amac_inner(v, *target)))
+        .collect::<Vec<_>>();
 
     let exe = LocalPool::from_futures(futs);
     exe.run_until_finish();
+}
+
+fn binary_search_amac_manual_execute(v: &[i32], targets: &[i32]) {
+    let mut futures = targets
+        .iter()
+        .map(|target| binary_search_amac_inner(v, *target))
+        .collect::<Vec<_>>();
+    let mut futs = Vec::new();
+    for fut in &mut futures {
+        let pf = unsafe { Pin::new_unchecked(fut) };
+        futs.push(pf);
+    }
+
+    while !futs.is_empty() {
+        for i in 0..futs.len() {
+            loop {
+                let mut bs = match futs.get_mut(i) {
+                    Some(bs) => bs,
+                    None => break,
+                };
+
+                match Pin::new(&mut bs).poll(&mut futures::task::Context::from_waker(
+                    futures::task::noop_waker_ref(),
+                )) {
+                    Poll::Pending => break,
+                    Poll::Ready(_) => {
+                        futs.swap_remove(i);
+                    }
+                }
+            }
+        }
+    }
 }
 
 async fn binary_search_amac_inner(v: &[i32], target: i32) {
@@ -77,9 +117,70 @@ async fn binary_search_amac_inner(v: &[i32], target: i32) {
     let _ = black_box(res);
 }
 
+fn binary_search_amac_generator_manual_execute(v: &[i32], targets: &[i32]) {
+    let mut gens = targets
+        .iter()
+        .map(|target| binary_search_amac_inner_generator(v, *target))
+        .collect::<Vec<_>>();
+    let mut bss = Vec::new();
+    for gen in &mut gens {
+        let bs = Pin::new(gen);
+        bss.push(bs);
+    }
+
+    while !bss.is_empty() {
+        for i in 0..bss.len() {
+            loop {
+                let mut bs = match bss.get_mut(i) {
+                    Some(bs) => bs,
+                    None => break,
+                };
+
+                match Pin::new(&mut bs).resume(()) {
+                    GeneratorState::Yielded(_) => break,
+                    GeneratorState::Complete(_) => {
+                        bss.swap_remove(i);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn binary_search_amac_inner_generator(
+    v: &[i32],
+    target: i32,
+) -> impl Generator<Yield = (), Return = ()> + '_ {
+    move || {
+        let mut left = 0;
+        let mut right = v.len();
+
+        while left < right {
+            let mid = (left + right) / 2;
+            let mid_ref = unsafe { v.get_unchecked(mid) };
+            prefetch(mid_ref);
+            yield;
+
+            if *mid_ref < target {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        let res = if left == v.len() {
+            None
+        } else {
+            Some(unsafe { v.get_unchecked(left) })
+        };
+
+        let _ = black_box(res);
+    }
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
-    let vec = gen_vec(1024 * 1024 * 1024);
-    let targets = (0..1000).map(|_| rand::random()).collect::<Vec<_>>();
+    let vec = gen_vec(256 * 1024 * 1024);
+    let targets = gen_vec(1000);
 
     let mut group = c.benchmark_group("binary_search");
 
@@ -93,9 +194,21 @@ fn criterion_benchmark(c: &mut Criterion) {
         })
     });
 
-    group.bench_function("amac", |b| {
+    group.bench_function("amac async boxed", |b| {
         b.iter_with_setup(setup, |_| {
             binary_search_amac(black_box(&vec), black_box(&targets))
+        })
+    });
+
+    group.bench_function("amac async unboxed", |b| {
+        b.iter_with_setup(setup, |_| {
+            binary_search_amac_manual_execute(black_box(&vec), black_box(&targets))
+        })
+    });
+
+    group.bench_function("amac generator unboxed", |b| {
+        b.iter_with_setup(setup, |_| {
+            binary_search_amac_generator_manual_execute(black_box(&vec), black_box(&targets))
         })
     });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 mod future;
 mod runtime;
 
-pub use future::PollOnce;
 pub use runtime::LocalPool;
 
 #[cfg(target_arch = "x86_64")]
+#[inline]
 pub fn prefetch<T>(reference: &T) {
     use std::arch::x86_64::{_mm_prefetch, _MM_HINT_NTA};
     let pointer: *const _ = &*reference;
     unsafe { _mm_prefetch(pointer as _, _MM_HINT_NTA) }
 }
 
+#[inline]
 pub async fn async_load<T>(r: &T) -> &T {
     prefetch(r);
     future::PollOnce::new().await;
